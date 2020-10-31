da094a8ebd10bce28a2d921e5ed3134c99e261e5
Brown <brownyogum@gmail.com>
2020-10-31 17:47:33 +0100

    console interface

--- launchModel.py
+++ launchModel.py
@@ -4,6 +4,7 @@ from visualisation.visualisation import *
 from multiprocessing import Process
 from tkinter import *
 import os
+import readchar
 
 
 
@@ -26,6 +27,9 @@ SETTINGS = [False, 'severe', 'severe', 'severe', 'severe']
 # index 3 = restrictions for adults
 # index 4 = restrictions for elderly
 
+RUNTYPE = 'single'
+
+LIVE_GRAPH_STATUS = True
 
 def MainProgram(simSettings, live_graph=True, vis=True, table=False, config=None):
     agents = simSettings[0]
@@ -113,18 +117,19 @@ def runSingle():
     agents = AGENTS
     infected = INIT_INFECTED
     simSettings = [agents, maskChance, infected, settings]
-    live_graph_status = True
+    # live_graph_status = True
 
-    if live_graph_status:
+    if LIVE_GRAPH_STATUS:
         f = open("visualisation/visual_data.txt", "w")
         f.close()
 
         p = Process(target=live_animation, args=[DAYS])
         p.start()
-        MainProgram(simSettings, live_graph=live_graph_status)
+        MainProgram(simSettings, live_graph=LIVE_GRAPH_STATUS)
         p.join()
 
-        os.remove("visualisation/visual_data.txt")
+        if os.path.isfile("visualisation/visual_data.txt"):
+            os.remove("visualisation/visual_data.txt")
 
     else:
         MainProgram(simSettings, live_graph=False)
@@ -158,8 +163,214 @@ def runComplete():
 	                    MainProgram(simSettings, live_graph=False, vis=False, table=True,config=config)
 
 
+error_message = ""
+
+def print_error_message():
+    global error_message
+    if error_message != "":
+        print("NOTE: "+error_message)
+        print("===================================\n")
+        error_message = ""
+
+def mapSettings(arguments):
+    global DAYS
+    global AGENTS
+    global INIT_INFECTED
+    global LIVE_GRAPH_STATUS
+    global SETTINGS
+    global MASKCHANCE
+    global error_message
+
+
+    # arguments = ''.join(arguments)
+    arguments= arguments.replace(" ","")
+    arguments = arguments.split(',')
+
+    for argument in arguments:
+        argument = argument.lower()
+        argument = argument.split('=')
+
+        if argument[0] == 'days':
+            if argument[1].isnumeric():
+                DAYS = int(argument[1])
+            else:
+                # print("Invalid number of Days entered, Using Default.")
+                error_message+="Invalid number of Days entered, Using Default.\n"
+        elif argument[0] == 'agents':
+            if argument[1].isnumeric():
+                AGENTS = int(argument[1])
+            else:
+                # print("Invalid number of Agents entered, Using Default.")
+                error_message+="Invalid number of Agents entered, Using Default.\n"
+        elif argument[0] == 'init_infected':
+            if argument[1].isnumeric():
+                INIT_INFECTED = int(argument[1])
+            else:
+                # print("Invalid number of Initial Infected entered, Using Default.")
+                error_message+="Invalid number of Initial Infected entered, Using Default.\n"
+        elif argument[0] == 'live_graph':
+            if argument[1] =='true':
+                LIVE_GRAPH_STATUS = True
+            elif argument[1] == 'false':
+                LIVE_GRAPH_STATUS = False
+            else:
+                # print("Invalid live graph status entered, Using Default.")
+                error_message +="Invalid live graph status entered, Using Default.\n"
+        elif argument[0] == 'school':
+            if (argument[1] == 'true'):
+                SETTINGS[0] = True
+            elif (argument[1] == 'false'):
+                SETTINGS[0] = False
+            else:
+                # print("Invalid school setting entered, Using Default.")
+                error_message+="Invalid school setting entered, Using Default.\n"
+        elif argument[0] == 'child':
+            if (argument[1] == 'none') or (argument[1] == 'minimal') or (argument[1] == 'moderate') or (argument[1] == 'severe') or (argument[1] == 'total'):
+                SETTINGS[1]=argument[1]
+            else:
+                # print("Invalid child lockdown setting entered, Using Default.")
+                error_message+="Invalid child lockdown setting entered, Using Default.\n"
+        elif argument[0] == 'young':
+            if (argument[1] == 'none') or (argument[1] == 'minimal') or (argument[1] == 'moderate') or (argument[1] == 'severe') or (argument[1] == 'total'):
+                SETTINGS[2]=argument[1]
+            else:
+                # print("Invalid young lockdown setting entered, Using Default.")
+                error_message+="Invalid young lockdown setting entered, Using Default.\n"
+
+
+        elif argument[0] == 'adult':
+            if (argument[1] == 'none') or (argument[1] == 'minimal') or (argument[1] == 'moderate') or (argument[1] == 'severe') or (argument[1] == 'total'):
+                SETTINGS[3]=argument[1]
+            else:
+                # print("Invalid adult lockdown setting entered, Using Default.")
+                error_message+="Invalid adult lockdown setting entered, Using Default.\n"
+        elif argument[0] == 'old':
+            if (argument[1] == 'none') or (argument[1] == 'minimal') or (argument[1] == 'moderate') or (argument[1] == 'severe') or (argument[1] == 'total'):
+                SETTINGS[4]=argument[1]
+            else:
+                # print("Invalid old lockdown setting entered, Using Default.")
+                error_message+="Invalid old lockdown setting entered, Using Default.\n"
+        elif argument[0] == 'mask_chance':
+            if (argument[1] == 'all') or (argument[1] == 'most') or (argument[1] == 'half') or (argument[1] == 'few') or (argument[1] == 'none'):
+                MASKCHANCE=argument[1]
+            else:
+                # print("Invalid MASK CHANCE setting entered, Using Default.")
+                error_message+="Invalid MASK CHANCE setting entered, Using Default.\n"
+
+        elif argument[0] != "":
+            # print(argument[0]+" is not a valid parameter")
+            error_message+=argument[0]+" is not a valid parameter\n"
+
+
+def displayHelp():
+    clear()
+    print("Use the format:\nparameter1 = value, parameter2 = value to set values to parameters")
+
+    print("The values of the following parameters can be changed:")
+    print("DAYS = INTEGER\nThis sets the number of days the simulation runs for and the value should be an integer")
+    print("-----------------------------------------")
+    print("AGENTS = INTEGER\nThis sets the popluation of the simulation and the value should be an integer")
+    print("-----------------------------------------")
+    print("init_infected = INTEGER\nThis sets the initial number of infected agents in the popluation and the value should be an integer")
+    print("-----------------------------------------")
+    print("mask_chance = VALUE\nThis sets what portion of the popluation use masks. The value can be: ALL, MOST, HALF, FEW or NONE")
+    print("school = BOOLEAN\nThis specifies if schools are open or not. The value can be: TRUE or FALSE")
+    print("-----------------------------------------")
+    print("child = VALUE\nThis specifies lockdown restrictions for children. The value can be: NONE, MINIMAL, MODERATE, SEVERE or TOTAL")
+    print("-----------------------------------------")
+    print("young = VALUE\nThis specifies lockdown restrictions for young adults. The value can be: NONE, MINIMAL, MODERATE, SEVERE or TOTAL")
+    print("-----------------------------------------")
+    print("adult = VALUE\nThis specifies lockdown restrictions for adults. The value can be: NONE, MINIMAL, MODERATE, SEVERE or TOTAL")
+    print("-----------------------------------------")
+    print("old = VALUE\nThis specifies lockdown restrictions for the elderly. The value can be: NONE, MINIMAL, MODERATE, SEVERE or TOTAL")
+    print("-----------------------------------------")
+    print("live_graph = BOOLEAN\nspecifies if a live graph should be used for visualisation of the simulation")
+
+    print("=====================================")
+    print("\nPress any button to go back to Settings")
+
+    x = readchar.readkey()
+
+    showSettings()
+
+
+def showSettings() :
+    clear()
+    print_error_message()
+    print("Number of days (days): ",DAYS)
+    print("Population (agents): ",AGENTS)
+    print("Initial number of infected (init_infected): ",INIT_INFECTED)
+    print("------------------------------")
+    print("Mask chance (mask_chance): ",MASKCHANCE)
+    print("School (school): ",SETTINGS[0])
+    print("------------------------------")
+    print("Children lockdown settings (child): ",SETTINGS[1])
+    print("Young Adult lockdown settings (young): ",SETTINGS[2])
+    print("Adult lockdown settings (adult): ",SETTINGS[3])
+    print("Elderly lockdown settings (old): ",SETTINGS[4])
+    print("------------------------------")
+    print("Live graphs: (live_graph)",LIVE_GRAPH_STATUS)
+    print("==============================")
+
+    print("\nPress 1 to go back to Main Menu")
+    print("Press 2 to change settings")
+    print("Press H for settings help")
+    print("Press Q to exit")
+
+
+    while True:
+        x = readchar.readkey()
+        if x == '1':
+            mainMenu()
+            break
+        elif x == '2':
+            print("Enter new settings: ")
+            arguments = input()
+            mapSettings(arguments)
+            showSettings()
+            break
+        elif x.lower() == 'q':
+            exit() 
+        elif x.lower() == 'h':
+            displayHelp()
+
+    
+    
+        
+def mainMenu():
+    clear()
+    print_error_message()
+    print("press 1 to proceed with simulation")
+    print("press 2 to view current settings")
+    print("press Q to quit ")
+
+    while True:
+        x = readchar.readkey()
+        if x == '1':
+            runSingle()
+            mainMenu()
+        if x == '2':
+            showSettings()
+            break
+        if x.lower() == 'q':
+            exit()    
+
+
+def clear():
+    if os.name in ('nt','dos'):
+        os.system("cls")
+    elif os.name in ('linux','osx','posix'):
+        os.system("clear")
+    else:
+        print("\n") * 120
+
 
 if __name__ == '__main__':
+    
+    arguments = ''.join(sys.argv[1:])
+    mapSettings(arguments)
 
-    runSingle()
+    mainMenu()
+    
+    
     #runComplete()
